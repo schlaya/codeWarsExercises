ea439e1d4ee59ae3371bc82b6c99778abf84c0e0
Liam Roth <roth.liam@protonmail.com>
2025-09-25 14:30:13 +0200

    auto commit

---
+++ gitPush.ts
@@ -0,0 +1,17 @@
+import { execSync } from "child_process";
+
+async function gitAdd() {
+    execSync("git add .");
+}
+
+async function gitCommit(message: string) {
+    execSync(`git commit -m "${message}"`);
+}
+
+async function gitPush() {
+    execSync("git push");
+}
+
+await gitAdd();
+await gitCommit("auto commit");
+await gitPush();
